77745e79c96ebb4fff2769f26072238caebaedc7
Siddharth Salunkhe <siddharth.salunkhe11@gmail.com>
2023-04-05 17:07:50 -0500

    generate python interaction data

--- scripts/create-text-response-python.py
+++ scripts/create-text-response-python.py
@@ -7,11 +7,11 @@ import os
 
 
 # open the excel file and read the contents into pandas dataframe
-df = pd.read_excel('milestone2-responses.xlsx', sheet_name='PYTHON')
+df = pd.read_excel('../milestone2-responses - Copy.xlsx', sheet_name='PYTHON')
 df
 print(df.head())
 
-number_of_rows_to_process = 15
+number_of_rows_to_process = 289
 
 
 
@@ -25,12 +25,12 @@ def generate_responses(df):
     # create a folder for each task
 
 
-    if not os.path.exists('python-responses/task1'):
-        os.makedirs('python-responses/task1')
-    if not os.path.exists('python-responses/task2'):
-        os.makedirs('python-responses/task2')
-    if not os.path.exists('python-responses/task3'):
-        os.makedirs('python-responses/task3')
+    if not os.path.exists('../python-responses/task1'):
+        os.makedirs('../python-responses/task1')
+    if not os.path.exists('../python-responses/task2'):
+        os.makedirs('../python-responses/task2')
+    if not os.path.exists('../python-responses/task3'):
+        os.makedirs('../python-responses/task3')
 
     # iterate over the rows of the dataframe from row 2
     for index, row in df.iterrows():  
@@ -40,8 +40,9 @@ def generate_responses(df):
             break
         #   if code column is not empty  and not Nan * prompt column does not contain the code
         filename = row['Filename']
+        # print(filename)
         # get the code from the file
-        code_file = open("sample-python-data/" + filename, "r")
+        code_file = open("../sample-python-data/" + str(filename), "r")
         code = code_file.read()
         # close the file
         code_file.close()
@@ -62,11 +63,11 @@ def generate_responses(df):
       
         if(task1 == True):
             # check if the file exists, if it does, append to it
-            if os.path.exists('python-responses/task1/code-' + str(codeindex) + '.txt'):
+            if os.path.exists('../python-responses/task1/code-' + str(codeindex) + '.txt'):
                 mode = 'a'
             else:
                 mode = 'w'
-            with open('python-responses/task1/code-' + str(codeindex) + '.txt', mode) as f:
+            with open('../python-responses/task1/code-' + str(codeindex) + '.txt', mode, encoding="utf8") as f:
                 # write promt in heading 6
                 f.write("\n-----------------------------------------\n")
                 f.write(" Prompt: " +prompt1 + '\n\n')
@@ -78,12 +79,12 @@ def generate_responses(df):
                 f.close()
         elif(task2 == True):
              # check if the file exists, if it does, append to it
-            if os.path.exists('python-responses/task2/code-' + str(codeindex) + '.txt'):
+            if os.path.exists('../python-responses/task2/code-' + str(codeindex) + '.txt'):
                 mode = 'a'
             else:
                 mode = 'w' 
 
-            with open('python-responses/task2/code-' + str(codeindex) + '.txt', mode) as f:
+            with open('../python-responses/task2/code-' + str(codeindex) + '.txt', mode,encoding="utf8") as f:
                 f.write("\n-----------------------------------------")
                 f.write( "\n Prompt: " +prompt1 + '\n')
                 f.write("-----------------------------------------\n")
@@ -92,16 +93,17 @@ def generate_responses(df):
                 f.close()
         elif(task3 == True):
             # check if the file exists, if it does, append to it
-            if os.path.exists('python-responses/task1/code-' + str(codeindex) + '.txt'):
+            if os.path.exists('../python-responses/task1/code-' + str(codeindex) + '.txt'):
                 mode = 'a'
             else:
                 mode = 'w'
-            with open('python-responses/task3/code-' + str(codeindex) + '.txt', mode) as f:
+            with open('../python-responses/task3/code-' + str(codeindex) + '.txt', mode, encoding="utf8") as f:
                 # write promt in heading 6
                 f.write("\n----------------------------------------")
                 f.write("\n Prompt: " +prompt1 + '\n')
                 f.write("-----------------------------------------\n")
                 f.write('ChatGPT response: ' + response + '\n')
+
 
                 f.close()
         else:
